ea6bbdf8d26966d6034ead28d150506a5e039cce
samiradh058 <adhikarisamir68@gmail.com>
2023-11-11 13:50:51 +0545

    First project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '120bb478-ac10-4c57-9d30-f4a031e7e5d2';
+export const MODAL_CLOSE_SEC = 2;
